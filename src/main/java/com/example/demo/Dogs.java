d94b66112ac0fe9d6405803e9ed07a706583d711
Meylin Borjas <meylincalix@icloud.com>
2025-10-08 23:38:09 -0400

    entity class

---
+++ src/main/java/com/example/demo/Dogs.java
@@ -0,0 +1,86 @@
+package com.example.demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "dogs")
+public class Dogs {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  private String breed;
+
+  @Column(nullable = false)
+  private int age;
+
+  private String description;
+
+  public Dogs() {
+  }
+
+  public Dogs(Long id, String name, String breed, int age, String description) {
+    this.id = id;
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+    this.description = description;
+  }
+
+  public Dogs(String name, String breed, int age, String description) {
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+    this.description = description;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getBreed() {
+    return breed;
+  }
+
+  public void setBreed(String breed) {
+    this.breed = breed;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
+
