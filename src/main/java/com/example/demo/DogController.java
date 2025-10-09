00f1c518e328ddb296298e242a9d5fa76355fa88
Meylin Borjas <meylincalix@icloud.com>
2025-10-08 23:44:24 -0400

    Dog controller java

---
+++ src/main/java/com/example/demo/DogController.java
@@ -0,0 +1,119 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DogController {
+    @Autowired
+  private DogService dogService;
+
+  /**
+   * Endpoint to get all dogs.
+   *
+   * @return List of all dogs.
+   */
+  @GetMapping("/dogs")
+  public Object getAllDogs() {
+    return dogService.getAllDogs();
+  }
+
+  /**
+   * Endpoint to get a dog by ID.
+   *
+   * @param id The ID of the dog to retrieve.
+   * @return The dog with the specified ID.
+   */
+  @GetMapping("/dogs/{id}")
+  public Dog getDogById(@PathVariable long id) {
+    return dogService.getDogById(id);
+  }
+
+  /**
+   * Endpoint to get dogs by breed.
+   *
+   * @param breed The breed of the dogs to search for.
+   * @return List of dogs with the specified breed.
+   */
+  @GetMapping("/dogs/breed/{breed}")
+  public Object getDogsByBreed(@PathVariable String breed) {
+    return dogService.getDogsByBreed(breed);
+  }
+
+  /**
+   * Endpoint to get dogs older than a specified age.
+   *
+   * @param age The minimum age of the dogs to retrieve.
+   * @return List of dogs older than the specified age.
+   */
+  @GetMapping("/dogs/olderThan")
+  public Object getDogsOlderThan(@RequestParam(name = "age", defaultValue = "5") int age) {
+    return new ResponseEntity<>(dogService.getDogsOlderThan(age), HttpStatus.OK);
+  }
+
+  /**
+   * Endpoint to add a new dog.
+   *
+   * @param dog The dog to add.
+   * @return The added dog.
+   */
+  @PostMapping("/dogs")
+  public Object addDog(@RequestBody Dog dog) {
+    return dogService.addDog(dog);
+  }
+
+  /**
+   * Endpoint to update an existing dog.
+   *
+   * @param id  The ID of the dog to update.
+   * @param dog The updated dog information.
+   * @return The updated dog.
+   */
+  @PutMapping("/dogs/{id}")
+  public Dog updateDog(@PathVariable Long id, @RequestBody Dog dog) {
+    dogService.updateDog(id, dog);
+    return dogService.getDogById(id);
+  }
+
+  /**
+   * Endpoint to delete a dog.
+   *
+   * @param id The ID of the dog to delete.
+   * @return List of remaining dogs.
+   */
+  @DeleteMapping("/dogs/{id}")
+  public Object deleteDog(@PathVariable Long id) {
+    dogService.deleteDog(id);
+    return dogService.getAllDogs();
+  }
+
+  /**
+   * Endpoint to write dog data to a JSON file.
+   *
+   * @param dog The dog to write to file.
+   * @return Confirmation message or file data.
+   */
+  @PostMapping("/dogs/writeFile")
+  public Object writeJson(@RequestBody Dog dog) {
+    return dogService.writeJson(dog);
+  }
+
+  /**
+   * Endpoint to read and return dog data from a JSON file.
+   *
+   * @return The contents of the JSON file.
+   */
+  @GetMapping("/dogs/readFile")
+  public Object readJson() {
+    return dogService.readJson();
+  }
+}
